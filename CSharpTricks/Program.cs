007555c435ce26a8d90e655ec93e0527302775fd
AlexLin <alex@zns.me>
2019-04-13 15:55:47 +0800

    ExpandoObject

---
+++ CSharpTricks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Dynamic;
+
+namespace CSharpTricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic test = new ExpandoObject();
+            test.someString = "hello";
+            test.someNumber = 1;
+            test.someFunction = new Action(() =>
+            {
+                Console.WriteLine($"param is :" + test.someString);
+                Console.WriteLine($"param2 is :" + test.someNumber);
+            });
+
+            test.someFunction();
+            Console.ReadLine();
+        }
+    }
+}
